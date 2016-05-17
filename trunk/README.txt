561907c58f39bd6421187ecb312fd84498a6fe1e
Mark Howells-Mead <permanent.tourist@gmail.com>
2016-05-17 10:51:45 +0200

    Tidy code

--- trunk/README.txt
+++ trunk/README.txt
@@ -4,7 +4,7 @@ Donate link: https://www.paypal.me/mhmli
 Tags: wp-admin, backend, list, user, create, creation date, sorting, column
 Requires at least: 4.3
 Tested up to: 4.5.2
-Stable tag: 1.1.3
+Stable tag: 1.1.3.1
 License: GPLv3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
@@ -19,6 +19,10 @@ sorted by this column.
 2. Activate the plugin through the 'Plugins' menu in WordPress
 
 == Changelog ==
+
+= 1.1.3 =
+* Confirm functionality in WordPress 4.5.2.
+* Clean code format. No functional change.
 
 = 1.1.3 =
 * Confirm functionality in WordPress 4.3.
--- trunk/index.php
+++ trunk/index.php
@@ -5,89 +5,101 @@ Plugin URI: https://wordpress.org/plugins/mhm-user-createdate/
 Text Domain: mhm-user-createdate
 Description: Adds a new column to the WordPress admin user list view, showing the date on which the user was created. The list can be sorted by this column.
 Author: Mark Howells-Mead
-Version: 1.1.3
+Version: 1.1.3.1
 Author URI: https://permanenttourist.ch/
 */
 
-class MHMUserCreatedate {
-
-    public $key     = '';
-    public $version = '1.1.3';
+class MHMUserCreatedate
+{
+    public $key = '';
+    public $version = '1.1.3.1';
     public $wpversion = '4.3';
 
-    public function __construct(){
-        register_activation_hook( __FILE__, array( $this, 'check_version' ) );
-        add_action( 'admin_init', array( $this, 'check_version' ) );
+    public function __construct()
+    {
+        register_activation_hook(__FILE__, array($this, 'check_version'));
+        add_action('admin_init', array($this, 'check_version'));
 
         // Don't run anything else in the plugin, if we're on an incompatible WordPress version
-        if ( ! $this->compatible_version() ) {
+        if (!$this->compatible_version()) {
             return;
         }
 
         $this->key = basename(__DIR__);
 
-        add_action( 'plugins_loaded', array($this, 'load_textdomain') );
-        add_filter( 'manage_users_columns', array($this, 'custom_column_header'), 20);
-        add_action( 'manage_users_custom_column', array($this, 'custom_column_content'), 10, 3);
-        add_filter( 'manage_users_sortable_columns', array($this, 'column_sortable'), 10, 1);
+        add_action('plugins_loaded', array($this, 'load_textdomain'));
+        add_filter('manage_users_columns', array($this, 'custom_column_header'), 20);
+        add_action('manage_users_custom_column', array($this, 'custom_column_content'), 10, 3);
+        add_filter('manage_users_sortable_columns', array($this, 'column_sortable'), 10, 1);
     }
 
-    public function check_version() {
+    public function check_version()
+    {
         // Check that this plugin is compatible with the current version of WordPress
-        if ( ! $this->compatible_version() ) {
-            if ( is_plugin_active( plugin_basename( __FILE__ ) ) ) {
-                deactivate_plugins( plugin_basename( __FILE__ ) );
-                add_action( 'admin_notices', array( $this, 'disabled_notice' ) );
-                if ( isset( $_GET['activate'] ) ) {
-                    unset( $_GET['activate'] );
+        if (!$this->compatible_version()) {
+            if (is_plugin_active(plugin_basename(__FILE__))) {
+                deactivate_plugins(plugin_basename(__FILE__));
+                add_action('admin_notices', array($this, 'disabled_notice'));
+                if (isset($_GET['activate'])) {
+                    unset($_GET['activate']);
                 }
             }
         }
     }
 
-    public function disabled_notice() {
+    public function disabled_notice()
+    {
         echo '<div class="notice notice-error is-dismissible">
-            <p>' .sprintf( __('The plugin “%1$s” requires WordPress %2$s or higher!', $this->key),
+            <p>'.sprintf(__('The plugin “%1$s” requires WordPress %2$s or higher!', $this->key),
                 _x('User creation date column in WordPress admin', 'Plugin name', $this->key),
-                $this->wpversion). '</p>
+                $this->wpversion).'</p>
         </div>';
     }
 
-    private function compatible_version() {
-        if ( version_compare( $GLOBALS['wp_version'], $this->wpversion, '<' ) ) {
+    private function compatible_version()
+    {
+        if (version_compare($GLOBALS['wp_version'], $this->wpversion, '<')) {
             return false;
         }
+
         return true;
     }
 
-	public function load_textdomain(){
-    	load_plugin_textdomain( $this->key, false, plugin_basename( dirname( __FILE__ ) ) . '/languages' );
+    public function load_textdomain()
+    {
+        load_plugin_textdomain($this->key, false, plugin_basename(dirname(__FILE__)).'/languages');
     }
 
-	public function custom_column_header($cols){
+    public function custom_column_header($cols)
+    {
         // Add column and header
-		$cols['mhm-user-createdate'] = __('Registration date', $this->key);
-		return $cols;
-	}
+        $cols['mhm-user-createdate'] = __('Registration date', $this->key);
 
-	public function custom_column_content($output, $column_name, $user_id){
+        return $cols;
+    }
+
+    public function custom_column_content($output, $column_name, $user_id)
+    {
         //	show content for each row
-		switch($column_name){
-			case 'mhm-user-createdate':
-			    $user = get_userdata( $user_id );
-                return strftime( __('%e.%m.%Y %H:%M:%S', $this->key), strtotime($user->user_registered));
+        switch ($column_name) {
+            case 'mhm-user-createdate':
+                $user = get_userdata($user_id);
+
+                return strftime(__('%e.%m.%Y %H:%M:%S', $this->key), strtotime($user->user_registered));
                 break;
         }
-        return $output;
-	}
 
-	public function column_sortable($columns) {
+        return $output;
+    }
+
+    public function column_sortable($columns)
+    {
         $custom = array(
             'mhm-user-createdate' => 'user_registered',
         );
+
         return wp_parse_args($custom, $columns);
     }
-
 }
 
 new MHMUserCreatedate();
